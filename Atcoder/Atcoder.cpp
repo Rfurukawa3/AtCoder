70071a7ddc0b22d3da8426a47e5cfe878d73552c
Furukawa Ryota <rfurukawa3@gmail.com>
2020-07-03 21:09:43 +0900

    全探索追加

--- Atcoder/Atcoder.cpp
+++ Atcoder/Atcoder.cpp
@@ -14,7 +14,6 @@ using ULL = unsigned long long;
 template<typename T> using V = vector<T>;
 template<typename T> using VV = vector<vector<T>>;
 template<typename T> using P = pair<T, T>;
-template<typename T> using VP = vector<pair<T, T>>;
 
 const double PI = 3.141592653589793;
 const int MOD = 1000000007;
@@ -29,49 +28,29 @@ template<> inline void col(double x) { cout << fixed << setprecision(12) << x <<
 
 signed main()
 {
-	int n, m;
-	cin >> n >> m;
-	vector<vector<int>> g(n);
-	for (int i = 0; i < m; i++)
-	{
-		int u, v;
-		cin >> u >> v;
-		u--; v--;
-		g[u].emplace_back(v);
-		g[v].emplace_back(u);
-	}
+	int n;
+	cin >> n;
+	vector<int> a(n), b(n);
+	for (auto& ai : a) cin >> ai;
+	for (auto& bi : b) cin >> bi;
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	vector<int> b0(b);
 
-	int ans = 0;
-	vector<bool> seen(n, false);
-	for (int i = 0; i < n; i++)
-	{
-		if (!seen[i]) {
-			// first := 現在のノード, second := 直前に訪問したノード
-			stack<pair<int,int>> st;
-			st.push({ i,-1 });
-			seen[i] = true;
-			bool roop = false;
-
-			while (!st.empty()) {
-				int now = st.top().first, prev = st.top().second;
-				st.pop();
-
-				for (auto& gi : g[now]) {
-					if (!seen[gi]) {
-						st.push({ gi, now });
-						seen[gi] = true;
-					}
-					else {
-						// 直前に訪問したわけでもないのに訪問済だったら閉路が存在する
-						if (gi != prev) roop = true;
-					}
-				}
+	int win = 0, all = 0;
+	do {
+		b = b0;
+		do {
+			int awin = 0, bwin = 0;
+			for (int i = 0; i < n; i++) {
+				if (a[i] > b[i]) awin++;
+				else bwin++;
 			}
+			if (awin > bwin) win++;
+			all++;
+		} while (next_permutation(b.begin(), b.end()));
+	} while (next_permutation(a.begin(), a.end()));
 
-			if (!roop) ans++;
-		}
-	}
-
-	cout << ans << endl;
+	cout << static_cast<double>(win) / all << '\n';
 	return 0;
 }
