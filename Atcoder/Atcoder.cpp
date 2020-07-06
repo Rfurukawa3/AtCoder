9013587e264d8e4681dd5c68de4c7ea6d0c556da
Furukawa Ryota <rfurukawa3@gmail.com>
2020-07-06 22:08:34 +0900

    貪欲法追加

--- Atcoder/Atcoder.cpp
+++ Atcoder/Atcoder.cpp
@@ -27,49 +27,30 @@ template<> inline void col(double x) { cout << fixed << setprecision(12) << x <<
 #pragma endregion
 
 signed main()
-{
-	int n, k;
-	string s;
-	cin >> n >> k >> s;
-	string smin(s), t(n, '.');
-	sort(smin.begin(), smin.end());
+{	
+	int n;
+	cin >> n;
+	vector<int> w(n);
+	for (auto& wi : w) cin >> wi;
 
-	int k0 = 0;
-	for (int i = 0; i < n; i++) { // t[i]を決める
-		for (int j = 0; j < n; j++) { // t[i]に入れる文字を小さい順に検証
-			if (smin[j] == '.') continue; // 使用済みか否かをチェック
-
-			// ktmp := t[i] = smin[j] とした時の最小の不一致数
-			int ktmp = k0;
-			if (s[i] != smin[j]) ktmp++;
-
-			// 未使用の文字がs[i+1]以降の文字列と何文字一致させられるか調べる
-			// abc1 := s[i+1]以降の文字列における各アルファベットの登場回数
-			// abc2 := 未使用の文字における各アルファベットの登場回数
-			vector<int> abc1(26, 0), abc2(26, 0);
-			for (int m = i+1; m < n; m++) {
-				abc1[s[m] - 'a']++;
-			}
-			for (int m = 0; m < n; m++) {
-				if (smin[m] == '.' || m == j) continue;
-				abc2[smin[m] - 'a']++;
-			}
-			int match = 0;
-			for (int m = 0; m < 26; m++) {
-				// abc1[0]=2, abc2[0]=4 の時、'a'2文字分は一致させられる
-				match += min(abc1[m], abc2[m]);
-			}
-
-			ktmp += n - i - 1 - match;
-			if (ktmp <= k) {
-				if (s[i] != smin[j]) k0++;
-				t[i] = smin[j];
-				smin[j] = '.';
+	vector<int> m;
+	m.emplace_back(w[0]);
+	for (int i = 1; i < n; i++) {
+		// 乗せられる段ボールの中で一番軽いものを選ぶのが最善
+		sort(m.begin(), m.end());
+		bool flg = true;
+		for (auto& mi : m) {
+			if (mi >= w[i]) {
+				mi = w[i];
+				flg = false;
 				break;
 			}
 		}
+		if (flg) {
+			m.emplace_back(w[i]);
+		}
 	}
 
-	cout << t << endl;
+	cout << m.size() << endl;
 	return 0;
 }
